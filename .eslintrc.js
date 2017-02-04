f917dc49dbde8b75e2e0914c864d0bb5eb47f9d3
vagrant <syuvashree1995@gmail.com>
2017-02-04 04:51:29 +0000

    final commit with d3

--- .eslintrc.js
+++ .eslintrc.js
@@ -141,7 +141,7 @@ module.exports = {
     "no-label-var": 2, // disallow labels that share a name with a variable
     "no-shadow": 2, // disallow declaration of variables already declared in the outer scope
     "no-shadow-restricted-names": 2, // disallow shadowing of names such as arguments
-    "no-undef": 2, // disallow use of undeclared variables unless mentioned in a /*global */ block
+    //"no-undef": 2, // disallow use of undeclared variables unless mentioned in a /*global */ block
     "no-undef-init": 2, // disallow use of undefined when initializing variables
     "no-undefined": 2, // disallow use of undefined variable (off by default)
     "no-unused-vars": 2, // disallow declaration of variables that are not used in the code
